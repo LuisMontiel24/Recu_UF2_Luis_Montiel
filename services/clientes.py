27a0284270a4d7bb119f92c8e67498307821081b
Angel Montiel <2024_luis.montiel@iticbcn.cat>
2025-06-02 18:55:20 +0200

    EXERCICI 2 Segons el formulari de la imatge, cal que el projecte, al rebre aquella informació, pugui inserir les dades a la taula corresponent.

---
+++ services/clientes.py
@@ -0,0 +1,15 @@
+from config.database import connect_db
+
+
+async def insertar_cliente(nombre, apellido, email, descripcion, curso, ano, direccion, postal, contrasena) -> dict:
+    with connect_db() as conn:
+        cur = conn.cursor()
+        sql = """
+        INSERT INTO clientes (nombre, apellido, email, descripcion, curso, ano, direccion, postal, contrasena)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        cur.execute(sql, (nombre, apellido, email, descripcion, curso, ano, direccion, postal, contrasena))
+        conn.commit()
+        return {"msg": "insertado "}
+
+
